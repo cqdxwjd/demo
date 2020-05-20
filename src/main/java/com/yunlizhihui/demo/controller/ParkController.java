307dd83e3ff6b8331e8768365da8a991fdb9b78f
cqdxwjd <cqdxwjd@163.com>
2020-05-20 10:16:59 +0800

    520

--- src/main/java/com/yunlizhihui/demo/controller/ParkController.java
+++ src/main/java/com/yunlizhihui/demo/controller/ParkController.java
@@ -1,26 +1,77 @@
 package com.yunlizhihui.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.yunli.bigdata.eventbus.sdk.AdminClient;
+import com.yunli.bigdata.eventbus.sdk.Event;
+import com.yunli.bigdata.eventbus.sdk.Producer;
+import com.yunli.bigdata.eventbus.sdk.Topic;
 import com.yunlizhihui.demo.domain.CarInfo;
 import com.yunlizhihui.demo.domain.ParkSlotStatus;
+import com.yunlizhihui.demo.utils.JDBCUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 public class ParkController {
 
-    @RequestMapping(value = "/car_info", method = RequestMethod.POST)
-    public boolean car_info(@RequestBody JSONObject jsonObject) {
+    //主机名
+    private String host = "localhost";
+    //端口
+    private int port = 8888;
+    //停车场车位topic
+    private final String park_topic = "slot_change";
+    //车辆进出停车场topic
+    private final String car_topic = "car_in_out";
+    //总线管理器
+    private AdminClient adminClient = new AdminClient(host, port);
+    //总线生产者
+    private Producer producer = new Producer(host, port) {
+        @Override
+        protected void onError(Throwable throwable) {
+            throwable.printStackTrace();
+        }
+    };
 
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(JDBCUtils.getDataSource());
+
+    /**
+     * 上传车辆进出信息
+     *
+     * @param jsonObject
+     * @return
+     */
+    @RequestMapping(value = "/car_info", method = RequestMethod.POST)
+    public boolean car_info(@RequestBody JSONObject jsonObject) throws InterruptedException, IOException {
+
+        //解析json对象
         CarInfo carInfo = jsonObject.toJavaObject(CarInfo.class);
+        //提取车位实时变化信息
+
+        //创建停车场车位topic
+        adminClient.createTopic(new Topic(park_topic, 1));
+        //向topic发送消息
+        producer.sendEventAsync(new Event(park_topic, "test", "data".getBytes()));
+
+        producer.flush();
+        producer.close();
+
+        //将carinfo对象存入mysql数据库
+        jdbcTemplate.update("xxx");
 
         return false;
     }
 
+    /**
+     * 上传停车场信息
+     *
+     * @param jsonObject
+     * @return
+     */
     @RequestMapping(value = "/park_slot", method = RequestMethod.POST)
     public boolean park_slot(@RequestBody JSONObject jsonObject) {
 
--- src/main/java/com/yunlizhihui/demo/service/Process.java
+++ src/main/java/com/yunlizhihui/demo/service/Process.java
@@ -1,7 +1,20 @@
 package com.yunlizhihui.demo.service;
 
+import com.yunli.bigdata.eventbus.sdk.AdminClient;
+import com.yunli.bigdata.eventbus.sdk.ConsumerGroup;
+import com.yunli.bigdata.eventbus.sdk.Event;
+import com.yunli.bigdata.eventbus.sdk.Topic;
 import com.yunli.toolkit.common.process.AbstractProcess;
 import com.yunli.toolkit.common.request.RequestImportCsvParams;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.fail;
 
 public class Process extends AbstractProcess {
     @Override
@@ -13,4 +26,72 @@ public class Process extends AbstractProcess {
     public void process() {
 
     }
+
+    @Test
+    void testConsumerProducerWithSDK() throws InterruptedException, ExecutionException, IOException {
+        final String topic = "foo";
+        final String group = "group1";
+
+        AdminClient adminClient = new AdminClient("localhost", 8888);
+        adminClient.createTopic(new Topic(topic, 1));
+        Thread.sleep(1000);
+        adminClient.registerConsumerGroup(new ConsumerGroup(group, topic));
+        Thread.sleep(1000);
+        assertEquals(1, adminClient.listTopics().size());
+        assertEquals(1, adminClient.listConsumerGroups(topic).size());
+
+        String desc = "1Go 2is 3an 4open 5source 6programming 7language 8that 9makes 10it 11easy 12to 13build 14simple, " +
+                "15reliable, 16and 17efficient 18software.";
+        String[] contents = desc.split("\\W+");
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        com.yunli.bigdata.eventbus.sdk.Consumer consumer = new com.yunli.bigdata.eventbus.sdk.Consumer("localhost",
+                8888,
+                new ConsumerGroup(group, topic),
+                Collections.singletonList(0)) {
+            private int index = 0;
+
+            @Override
+            protected void onError(Throwable t) {
+                t.printStackTrace();
+                fail();
+            }
+
+            @Override
+            protected void onEvent(Event e) {
+                assertEquals(contents[index], new String(e.getData()));
+                index++;
+                if (index == contents.length) {
+                    latch.countDown();
+                }
+            }
+        };
+        Thread.sleep(1000);
+
+        com.yunli.bigdata.eventbus.sdk.Producer producer = new com.yunli.bigdata.eventbus.sdk.Producer("localhost",
+                8888) {
+            @Override
+            protected void onError(Throwable t) {
+                t.printStackTrace();
+                fail();
+            }
+        };
+        for (String content : contents) {
+            Event event = new Event(topic, content, content.getBytes());
+            producer.sendEventAsync(event);
+        }
+        producer.flush();
+        producer.close();
+        latch.await();
+        consumer.close();
+
+        adminClient.unregisterConsumerGroup(new ConsumerGroup(group, topic));
+        Thread.sleep(1000);
+        adminClient.dropTopic(topic);
+
+        assertEquals(0, adminClient.listTopics().size());
+        adminClient.close();
+    }
+
 }
